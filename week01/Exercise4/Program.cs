ace87928f365222b796df8069636a90f1c978887
Ivan Suarez <sua230009@byui.edu>
2025-05-09 02:25:51 -0500

    List exercise

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+         List<int> numbers = new List<int>();
+        int num = 1;
+        int sum = 0;
+        double average = 9;
+        int bigger = int.MinValue;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (num != 0){
+            string num_entered = Console.ReadLine();
+            num = int.Parse(num_entered);
+
+            if (num != 0)
+            {
+                numbers.Add(num);
+            }  
+        }
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if(number > bigger){
+                    bigger = number;
+                }
+            
+        }
+        
+        int amount = numbers.Count;
+        if (amount > 0)
+        {
+            average = sum / amount;
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {bigger}");
+        }
     }
 }
