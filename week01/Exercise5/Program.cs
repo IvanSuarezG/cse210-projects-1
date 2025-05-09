7b03f41937109c3ca7ee0296ca7f2aab983c1981
Ivan Suarez <sua230009@byui.edu>
2025-05-09 02:58:47 -0500

    fuctions exercise

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int couple = SquareNumber(number);
+        DisplayResult(couple, name);
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name?  ");
+        string _name = Console.ReadLine();
+        return _name;
+    }
+
+    static int PromptUserNumber()
+    {
+        int number;
+        while (true)
+        {
+            Console.Write("Please enter your favorite number: ");
+            string num = Console.ReadLine();
+            number = int.Parse(num);
+            return number;
+        }
+
+    }
+
+    static int SquareNumber(int value)
+    {
+        int square = value * value;
+        return square;
+    }
+
+    static void DisplayResult(int amount, string UserName)
+    {
+        DisplayWelcome();
+        Console.WriteLine($"{UserName}, The square of your number is {amount}");
+
     }
 }
