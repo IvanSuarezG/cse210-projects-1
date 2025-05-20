c720502f43f627c98ce8866f043a85ca81a902c5
Ivan Suarez <sua230009@byui.edu>
2025-05-20 17:47:55 -0500

    Fraction practica

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -17,10 +17,42 @@ public class Fraction
         _bottom = 1;
     }
 
+
+
     public Fraction(int top, int bottom)
     {
         _top = top;
         _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
     }
 
 }
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -7,9 +7,19 @@ class Program
         Fraction f1 = new Fraction();
         Fraction f2 = new Fraction(5);
         Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction();
+        f4.SetBottom(3);
 
-        Console.WriteLine($"f1: {f1}");
-        Console.WriteLine($"f2: {f2}");
-        Console.WriteLine($"f3: {f3}");
+
+        Console.WriteLine($"f1: {f1.GetFractionString()}");
+        Console.WriteLine($"f1: {f1.GetDecimalValue()}");
+        Console.WriteLine($"f2: {f2.GetFractionString()}");
+        Console.WriteLine($"f2: {f2.GetDecimalValue()}");
+        Console.WriteLine($"f3: {f3.GetFractionString()}");
+        Console.WriteLine($"f3: {f3.GetDecimalValue()}");
+        Console.WriteLine($"f4: {f4.GetFractionString()}");
+        Console.WriteLine($"f4: {f4.GetDecimalValue()}");
+
+
     }
 }
