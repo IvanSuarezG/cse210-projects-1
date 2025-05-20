1ed5f2aa96feb6ef15fc4f31b404a0472ec4b42d
Ivan Suarez <sua230009@byui.edu>
2025-05-20 17:21:19 -0500

    Fraction practice

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+
+        Console.WriteLine($"f1: {f1}");
+        Console.WriteLine($"f2: {f2}");
+        Console.WriteLine($"f3: {f3}");
     }
 }
